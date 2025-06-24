ae1d9338e177608c952eba4491604706c2ea753e
juhigupta0 <juhigupta262@gmail.com>
2025-06-24 15:18:13 +0530

    Day 1: Historian Hysteria

---
+++ Day_01/day_01.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    let filename = "/home/juhi/etn-electrical/advent_2024/src/input.txt";
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut vecLeft = Vec::new();
+    let mut vecRight = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut nums = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok());
+        if let (Some(left), Some(right)) = (nums.next(), nums.next()) {
+            vecLeft.push(left);
+            vecRight.push(right);
+        }
+    }
+
+    vecLeft.sort_unstable();
+    vecRight.sort_unstable();
+
+    let sum: i32 = vecLeft
+        .iter()
+        .zip(vecRight.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+
+    println!("Sum: {}", sum);
+
+    Ok(())
+}
